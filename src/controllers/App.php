ea4d6da58082fb4a36b47f8d4f85463fb4e3a615
Nilton Duarte <tvirapegubeco@gmail.com>
2022-09-19 17:35:43 -0300

    --

--- src/controllers/App.php
+++ src/controllers/App.php
@@ -21,6 +21,7 @@ class App
 
     /** @var null|User */
     private ?User $user;
+    private Application $application;
 
     public function __construct()
     {
@@ -39,6 +40,7 @@ class App
         $this->user->isId(Auth::user()->id);
         $this->user->isToken(Auth::user()->token);
         $this->user->isPhoto(Auth::user()->photo);
+        $this->application = new Application();
     }
 
     /**
@@ -236,16 +238,7 @@ class App
      */
     public function toHiddenArticle(array $data): void
     {
-        if (isset($data['id'])) {
-            $id = filter_var($data['id'], FILTER_VALIDATE_INT);
-            $article = (new Post())->isId($id);
-            $currentStatus = (new Model($article))->read(['id'], ['hidden'])->first()->hidden;
-            $status = ($currentStatus == 1 ? '0' : '1');
-            $modelUpdate = (new Model($article))->update('id = :id', ['id' => $id], ['hidden' => $status]);
-            echo json_encode(['success' => $modelUpdate]);
-            return;
-        }
-        echo json_encode(['success' => 0]);
+        $this->application->toHiddenArticle($data);
     }
 
     /**
@@ -369,51 +362,12 @@ class App
      */
     public function commentAction(): void
     {
-        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
-        if ($post) {
-            $comment = (new Comment())->isId($post['id']);
-            $comentModel = (new Model($comment))->read(['id'])->first();
-            if (is_null($comentModel)) {
-                echo json_encode(['error' => 'comentário inexistente.']);
-                return;
-            }
-            if ($post['action'] === 'edit') {
-                $comentModel = (new Model($comment))->update('id = :n', ['n' => $post['id']], ['text' => $post['text']]);
-                echo json_encode(['success' => 'comentário atualizado.']);
-                return;
-            }
-            if ($post['action'] === 'delete') {
-                $comentModel = (new Model($comment))->delete('id = :n', ['n' => $post['id']]);
-                echo json_encode(['success' => 'comentário deletado.']);
-                return;
-            }
-        }
+        $this->application->commentAction();
     }
 
     public function replyActions(): void
     {
-        $id = filter_input(INPUT_POST, 'repply_id', FILTER_SANITIZE_NUMBER_INT);
-        $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
-        if (!$id) {
-            echo json_encode(['error' => true, 'describe' => 'id inexistente']);
-            return;
-        }
-        if ($action === 'delete') {
-            $repply = (new CommentsReply())->isId($id);
-            if ((new Model($repply))->delete('id = :i', ['i' => $id])) {
-                echo json_encode(['success' => true]);
-                return;
-            }
-        }
-        if ($action === 'edit') {
-            $text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_SPECIAL_CHARS);
-            $repply = (new CommentsReply())->isId($id)->isText($text);
-            if ((new Model($repply))->update('id = :n', ['n' => $id], ['text' => $text])) {
-                echo json_encode(['success' => true]);
-                return;
-            }
-        }
-        echo json_encode(['success' => false]);
+        $this->application->replyActions();
     }
 
     /**
